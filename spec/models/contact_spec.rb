0eeede98e9396ef6ac52aa23f3050d351e54e96b
oyuk <okysoft68@gmail.com>
2016-10-15 23:46:57 +0900

    chapter 4

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,43 +12,35 @@ describe Contact do
   end
 
   it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   it "is invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   it "is invalid without an email address" do
-    contact = Contact.new(email: nil)
+    contact = build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: "Joe",
-      lastname: "Tester",
-      email: "tester@example.com"
-    )
-    contact = Contact.create(
-      firstname: "Jane",
-      lastname: "Tester",
-      email: "tester@example.com"
-    )
+    email = 'aaron@example.com'
+    create(:contact, email: email)
+    contact = build(:contact, email: email)
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
   it "returns a contact's full name as a string" do
-    contact = Contact.create(
+    contact = build(:contact,
       firstname: "Joe",
       lastname: "Tester",
-      email: "tester@example.com"
     )
     expect(contact.name).to eq "Joe Tester"
   end
@@ -85,6 +77,14 @@ describe Contact do
       end
     end
 
+  end
+
+  it "has a valid factory" do
+    expect(build(:contact)).to be_valid
+  end
+
+  it "has three phone numbers" do
+    expect(create(:contact).phones.count).to eq 3
   end
 
 end
